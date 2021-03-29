9dda2dc5ee792f08c468e2d660a48b0cc128fd64
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2021-03-29 21:36:14 +0300

    Оно не работает

--- Lab8.py
+++ Lab8.py
@@ -4,25 +4,42 @@ from matplotlib import pyplot as plt
 from scipy import signal
 import math as m
 
+def find_ind(data,left, right):
+    far = []
+    this_data = data
+    for i in range(1024):
+        if left <= this_data[i] <= right or left >= this_data[i] >= right:
+            far.append(i)
+    return far
+
+def find_up_bound(data, bound):
+    far = []
+    this_data = data
+    for i in range(1024):
+        if bound >= this_data[i]:
+            far.append(i)
+    return far
+
 def fisher(k_d, begin,end):
-    #k_d = 7
     delt = (end - begin + 1) // k_d
     glsum = 0
-    srarr = []
-    for k in range(k_d - 1):
+    meanarr = []
+    for k in range(k_d):
         ar = dffilt[(begin + k * delt): (begin + (k + 1) * delt)]
-        sr = np.mean(ar)
-        srarr.append(sr)
-        glsum += sum([(ar[i] - sr) ** 2 for i in range(len(ar))])
+        meanarr.append(np.mean(ar))
+        glsum += sum([(ar[i] - np.mean(ar)) ** 2 for i in range(len(ar))])
 
     s_inta = glsum / k_d / (k_d - 1)
-    x_sr = np.mean(srarr)
+    x_gl_mean = np.mean(meanarr)
 
-    s_inter = sum([(srarr[j] - x_sr) ** 2 for j in range(len(srarr))]) * k_d / (k_d - 1)
-    print(round(s_inter / s_inta,4))
+    s_inter = sum([(meanarr[j] - x_gl_mean) ** 2 for j in range(len(meanarr))]) * k_d / (k_d - 1)
+    print(round(s_inter / s_inta, 4))
     return s_inter / s_inta
 
 
+NUM = 543
+
+# чтение из файла данных и перевод их в человеческий вид (double)
 f = open('D:/Downloads/wave_ampl.txt', 'r')
 lines = []
 for line in f:
@@ -32,32 +49,43 @@ lines[0] = lines[0].replace(']', '')
 data = lines[0].split(', ')
 dataf = [float(data[i]) for i in range(len(data))]
 
-t = np.linspace(0, len(dataf), len(dataf), endpoint=False)
-
+# print all data (butterfly)
+#t = np.linspace(0, len(dataf), len(dataf), endpoint=False)
 # plt.plot(t, dataf)
 # plt.show()
+
+#print signal
 fig, ax = plt.subplots(1, 1)
 t1024 = np.linspace(0, 1024, 1024, endpoint=False)
-plt.plot(t1024, dataf[0+1024:1024*2])
-plt.show()
-#counts, bins = np.histogram(dataf[0+1024:(0+1024+1024)])
-#ax.hist(bins[:-1], bins, weights=counts, alpha=0.5, color='g')
-
-print(len(dataf))
-dffilt = signal.medfilt(dataf[0 + 1024:1024 * 2], kernel_size=15)
-plt.plot(t1024, dffilt,color='g')
+plt.plot(t1024, dataf[1024 * NUM: 1024 * (NUM + 1)])
 plt.grid()
 plt.show()
-# plt.plot(283,dffilt[283], 'ro')
-delta_array = [m.fabs(dffilt[i] - dffilt[i + 1]) for i in range(1023)]
 
-ind = []
-for i in range(len(delta_array)):
-    if delta_array[i] > 0.005:
-        ind.append(i)
-a = [0, 258, 310, 744, 800, 1023]
-# plt.plot(ind, dffilt[ind], 'ro') #1,258,310,744,800,1024
+#print histogram
+fig1, ax1 = plt.subplots(1, 1)
+counts, bins = np.histogram(dataf[1024 * NUM: 1024 * (NUM + 1)])
+ax1.hist(bins[:-1], bins, weights=counts, alpha=0.6, color='g')
+plt.grid()
+plt.show()
 
+#print filtered signal
+dffilt = signal.medfilt(dataf[1024 * NUM: 1024 * (NUM + 1)], kernel_size=5)
+plt.plot(np.linspace(0, 1023, 1024, endpoint=False), dffilt,color='g')
+plt.grid()
+plt.show()
+
+#searsh for boundaries
+ind_signal = find_ind(dataf[1024 * NUM: 1024 * (NUM + 1)],bins[0],bins[1])
+
+ind_ground = find_ind(dataf[1024 * NUM: 1024 * (NUM + 1)],bins[len(bins) - 2],bins[len(bins) - 1])
+
+for i in range(len(ind_ground)-1):
+    if ind_ground[i+1] - ind_ground[i] > 1:
+        left = ind_ground[i]
+        right = ind_ground[i+1]
+
+
+a=[0,left,ind_signal[0], ind_signal[len(ind_signal)-1],right,1023]
 for i in range(len(a)-1):
     if (i == 0) or (i == 4):
         col = 'b'
@@ -68,10 +96,10 @@ for i in range(len(a)-1):
     if i == 2:
         col = 'r'
         lab = 'Сигнал'
-    plt.plot(np.linspace(a[i] + 1, a[i+1], a[i+1] - a[i] - 1, endpoint=True), dffilt[a[i]+1:a[i+1]], color=col, label = lab)
+    plt.plot(np.linspace(a[i] + 1, a[i+1], a[i+1] - a[i] - 1), dffilt[a[i]+1:a[i+1]], color=col, label = lab)
 
 # типа поделить на 7 для каждого отднльно
-k_d = 7
+k_d = 8
 fisher(7,a[0],a[1])
 fisher(4,a[1],a[2])
 fisher(4,a[2],a[3])
